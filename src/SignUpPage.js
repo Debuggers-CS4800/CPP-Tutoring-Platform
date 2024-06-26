66680907162788ed3dd06f1dbf9e77539fe0a8f0
Layla Razvi <lrazvi@oxy.edu>
2024-06-25 23:40:30 -0700

    signup page

---
+++ src/SignUpPage.js
@@ -0,0 +1,102 @@
+import React from "react"
+import { useState } from "react";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+
+
+
+const SignupComponent = ({isOpen, onClose}) => {
+    const [emailController, setEmail] = useState("");
+    const [passwordController, setPassword] = useState("");
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+        apiKey: "AIzaSyB-jsQGHmxCDjf9k0SWMrjsS6R1E9gs4QI",
+        authDomain: "cs4800-cpp-tutorplatform.firebaseapp.com",
+        projectId: "cs4800-cpp-tutorplatform",
+        storageBucket: "cs4800-cpp-tutorplatform.appspot.com",
+        messagingSenderId: "692396275639",
+        appId: "1:692396275639:web:7bd1c939063bdda32b1a32"
+    };
+  
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
+    const styling = {padding: "8%", 
+        height: "2%", 
+        display: "flex",
+        alignItems: "center"
+    }
+
+    const authenticate = () => {
+        createUserWithEmailAndPassword(auth, emailController, passwordController)
+        .then((userCredential) => {
+            // Signed up 
+            const user = userCredential.user;
+            console.log(user);
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ..
+        });
+    }
+    
+
+    if (!isOpen) return null;
+    return(
+       <div
+        style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            background: "rgba(0, 0, 0, 0.5)",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+       }}>
+            <button onClick={onClose}>x</button>
+            <div
+                style={{
+                    background: "white",
+                    height: 170,
+                    width: 240,
+                    margin: "auto",
+                    padding: "7%",
+                    border: "2px solid #000",
+                    borderRadius: "10px",
+                    boxShadow: "2px solid black",
+                    display: "flex",
+                    flexWrap: "wrap",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}>
+                    <h2>Sign Up</h2>
+                    <input
+                        style={styling} 
+                        placeholder="CPP Email"
+                        value={emailController}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <input
+                        placeholder="Password"
+                        style={styling}
+                        value={passwordController}
+                        onChange={(p) => setPassword(p.target.value)}
+                    />
+                    <button 
+                        style={styling}
+                        onClick={authenticate}
+                    >Sign Up</button>
+            </div>
+       </div>
+    ); 
+}
+
+
+export default SignupComponent;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,30 @@
 import React from 'react';
+import { useState } from 'react';
 import './Header.css';
+import '../SignUpPage';
+import { Link } from 'react-router-dom';
+import SignupComponent from '../SignUpPage';
 
 function Header() {
+    const [open, setOpen] = useState(false);
+    const signup = () => {
+        return(null)
+    }
+    
     return (
     <header className="header">
         <img src = "https://www.cpp.edu/brand/img/thumb-primary-logo-horizontal-stacked.jpg"/>
         <div className="logo">
             CPP-Tutoring Hub
         </div>
-        <nav className="nav">
-        <a href="#search">Search</a>
-        <a href="#about">About</a>
-        <a href="#login">Login</a>
-        <a href="#help">Help</a>
+            <nav className="nav">
+            <a href="#search">Search</a>
+            <a href="#about">About</a>
+            {/* <Link to= "/SignUpPage">Login</Link> */}
+            <a href='#search' onClick={() => setOpen(true)}>Login</a>
+            <a href="#help">Help</a>
         </nav>
+        <SignupComponent isOpen={open} onClose={() => setOpen(false)}></SignupComponent>
     </header>
     );
 }

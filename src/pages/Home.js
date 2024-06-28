33debb361f122399d66e2856394a47abf05ad608
Johnny Fok <55413097+JohnFok119@users.noreply.github.com>
2024-06-27 18:57:37 -0700

    merging jfok branch to main (#5)
    * Edited and adjusted CSS and home age routing
    
    -Added a Home Button on the Header
    -Edited css for Login & Sign up page
    - edited css for About page
    -Removed Help Page and its routing on App.js
    
    * edited README formatting
    
    * fixed README
    
    * -added home page route to listing
    -added stand-in data for listing
    -added grid of items for tutor info
    
    ---------
    
    Co-authored-by: JohnFok119 <johnny.y.fok@gmail.com>

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,6 +16,7 @@ function Home() {
                   subject="Math"
                   description="Experienced educators with advanced degrees in mathematics and related fields. Our tutors specialize in college-level calculus, providing personalized instruction to help you master complex concepts and excel in your studies."
                   linkText="Find Math Tutors Here!"
+                  linkTo="/search"
                   subjectImage="https://www.hlmathemagic.com/storage/2019/10/153-1538667_lfsf-ort-upcoming-math-transparent-background-mathematics-illustration.png"
                 />
               <hr />
@@ -23,6 +24,7 @@ function Home() {
                   subject="English/Arts"
                   description="Accomplished scholars specializing in English literature and the arts, bringing advanced degrees and extensive experience to support your mastery of literary analysis, writing skills, and creative projects."
                   linkText="Find English Tutors Here!"
+                  linkTo="/search"
                   subjectImage="https://www.ogdensd.org/uploaded/photos/Website_Icons/176-open-book.png"
                 />
               <hr />
@@ -30,6 +32,7 @@ function Home() {
                   subject="Science/Engineering"
                   description="CPP provides knowledgeable educators with advanced degrees in science and engineering disciplines. Our tutors specialize in guiding students through complex scientific and engineering concepts, offering personalized support to excel in coursework and projects."
                   linkText="Find Science Tutors Here!"
+                  linkTo="/search"
                   subjectImage="https://lh3.googleusercontent.com/proxy/Ki7Ep4UGFLQUIYe3Q4BlL50qi7rqGiL9XMgDDJSRLsIc3aze82aPkIwCyjP3HkdQqJg9TPL57K8q8kZPk2Og46FMaulR7CrlaaKKA6plX4PECd3MCHoPgV5THdP2"
                 />
               </main>
